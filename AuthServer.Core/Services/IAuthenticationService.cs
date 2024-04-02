c6b4bf7dbf2caeb31e2a22952ba4f13e7a0ed6ed
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2024-04-03 02:37:24 +0300

    IAuthenticationService interface'sinin oluşturulması

--- AuthServer.Core/Services/IAuthenticationService.cs
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AuthServer.Core.Dtos;
+using AuthServer.Core.DTOs;
 
 namespace AuthServer.Core.Services
 {
